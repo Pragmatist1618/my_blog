363212598cd712f208ae9a7d81da788f9237a2ef
pragmatist1618 <pragmatist1618@gmail.com>
2021-09-16 16:05:45 +0300

    frontend update

--- blog/forms.py
+++ blog/forms.py
@@ -8,8 +8,29 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
+    # def __init__(self, *args, **kwargs):
+    #     super(Co, self).__init__(*args, **kwargs)
+    #     # self.name.widget.attrs.update({'class': 'form-control'})
+    #     # self.fields['name'].widget.attrs.update({'class': 'form-control'})
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
+
         fields = ('name', 'email', 'body')
+        labels = {
+            'name': 'Имя',
+            'email': 'E-mail',
+            'body': 'Сообщение',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
+
+
+
+
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -151,3 +151,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # при заданной настройке email сообщения выводятся в консоль
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
